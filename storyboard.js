8e67aca71c7072aa0ca4fb4d40d441d3b368a46a
evanzummeren <erikvanzummeren@gmail.com>
2020-03-31 17:36:44 -0400

    update

--- storyboard.js
+++ storyboard.js
@@ -3,30 +3,60 @@
 /**
  * @param {string} spec – A string containing the playerStoryboardSpecRenderer.spec
  * @param {boolean} hq – High quality true
+ * @param {number} seconds – Duration of the YouTube video
  */
-module.exports = function storyboard(spec, hq, seconds) {
+module.exports = function storyboard(returnType, spec, hq, seconds) {
   let specParts = spec.split('|');
-  let specPartsLength = specParts.length -1;
   let baseUrlHq = specParts[0].split('$')[0] + '2/';
   let baseUrlLq = specParts[0].split('$')[0] + '1/';
   let sgpPart = specParts[0].split('$N')[1];
 
-  // console.log(specPartsLength);
+  let sighPartHq;
+  let sighPartLq;
 
-  let sighPartHq = specParts[specPartsLength].split('M#')[1];
-  let sighPartLq = specParts[specPartsLength-1].split('M#')[1];
+  console.log(specParts.length)
+
+  if(specParts.length === 3 ) {
+    sighPartHq = specParts[2].split('M#')[1];
+    sighPartLq = specParts[1].split('M#')[1];
+  } else {
+    sighPartHq = specParts[3].split('M#')[1];
+    sighPartLq = specParts[2].split('M#')[1];
+  }
+
+  let amountOfBoards = 0;
+  let division = 25;
 
   let storyboardArray = [];
 
+  if (hq === false) { division = 100 }; /* More frames per image in the case of LQ */
+
+  if (seconds < 250) {
+    amountOfBoards = Math.ceil((seconds / 2)/division);
+  } else if (seconds > 250 && seconds < 1000) {
+    amountOfBoards = Math.ceil((seconds / 4)/division);
+  } else if (seconds > 1000) {
+    amountOfBoards = Math.ceil((seconds / 10)/division);
+  } 
+
   if (hq === true) { /* High quality storyboard */
-    for (let i = 0; i < specPartsLength; i++) {
+    for (let i = 0; i < amountOfBoards; i++) {
       storyboardArray.push(baseUrlHq + 'M' + i + sgpPart + '&sigh=' + sighPartHq)
     }
   } else { /* Low quality storyboard */
-    for (let i = 0; i < specPartsLength; i++) {
+    for (let i = 0; i < amountOfBoards; i++) {
       storyboardArray.push(baseUrlLq + 'M' + i + sgpPart + '&sigh=' + sighPartLq)
     }
   }
 
-  return storyboardArray;
+  if(returnType === "url") {
+    return storyboardArray;
+  } else if (returnType === "parts") {
+    let obj = {
+      firstPart: `${baseUrlHq}M`,
+      secondPart: `${sgpPart}&sigh=${sighPartHq}`
+    }
+
+    return obj;
+  }
 }
