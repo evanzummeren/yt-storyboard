bc6a8ac2c9cc3d2c4dba00e27bc279d15eb818ba
evanzummeren <erikvanzummeren@gmail.com>
2020-03-31 15:56:19 -0400

    rm index

--- index.js
+++
@@ -1,3 +0,0 @@
-const storyboard = require('./storyboard.js');
-
-console.log(storyboard('https://i9.ytimg.com/sb/0iZwaV3RNbI/storyboard3_L$L/$N.jpg?sqp=ovOX_wMGCPSdlY4F|48#27#100#10#10#0#default#rs$AOn4CLDzsRhz_6H9qWvXvjrnLX9L25j6nQ|60#45#328#10#10#10000#M$M#rs$AOn4CLD4S-PdxIOa1pKXgTRGzBCWcJRm6Q|120#90#328#5#5#10000#M$M#rs$AOn4CLA1lnZQ4-T-Pi6_HGdE0G9R6ceHHQ', true));
